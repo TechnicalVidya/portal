8ae551c14f280b2eea02913795a1549bfd4f2841
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-07 23:45:11 +0530

    Added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
