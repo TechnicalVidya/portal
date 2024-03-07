d982e034eb820cc66120e2adf43d1efde12af649
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-07 23:50:04 +0530

    Added Backend URI

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 import withPWAInit from "@ducanh2912/next-pwa";
+const API_URL = process.env.API_URL;
 
 const withPWA = withPWAInit({
   dest: "public",
@@ -8,11 +9,21 @@ const withPWA = withPWAInit({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
-  disable: false,
+  disable: process.env.NODE_ENV === "development",
+  // disable: false,
   workboxOptions: {
     disableDevLogs: true,
   },
 });
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/api/:path*`,
+      },
+    ];
+  },
+};
 
 export default withPWA(nextConfig);
