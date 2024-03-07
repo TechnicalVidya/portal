4b5017eeed1fa155f7eb05ec061735b4e9dfb5dd
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-07 22:41:49 +0530

    Added: Shardcn

---
+++ src/components/heroComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const HeroComponent = () => {
+  return (
+    <div>
+      {/* <Button>jhvs</Button> */}
+    </div>
+  );
+};
+
+export default HeroComponent;
---
+++ src/modules/home/index.js
@@ -0,0 +1,12 @@
+import HeroComponent from "@/components/heroComponent";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroComponent />
+    </div>
+  );
+};
+
+export default Home;
