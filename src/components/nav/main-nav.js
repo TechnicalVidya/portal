e0cd7f10e355e0d469e57ae743876c0db1c50814
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-10 12:09:54 +0530

    Completed Nav

--- src/components/nav/main-nav.js
+++ src/components/nav/main-nav.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
 import { siteConfig } from "@/config/site";
@@ -6,6 +7,12 @@ import { cn } from "@/lib/utils";
 import { Icons } from "./icons";
 
 function MainNav({ items }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleLinkClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2" passHref>
@@ -21,12 +28,15 @@ function MainNav({ items }) {
                   key={index}
                   href={item.href}
                   className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground",
+                    "flex items-center text-sm font-medium",
+                    activeIndex === index
+                      ? "text-foreground"
+                      : "text-muted-foreground",
                     item.disabled && "cursor-not-allowed opacity-80"
                   )}
+                  onClick={() => handleLinkClick(index)}
                   passHref
                 >
-                  {/* {console.log(item)} */}
                   {item.title}
                 </Link>
               )
