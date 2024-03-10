14b839999e337d4c6696c59e33f7f33e755c5162
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-10 11:49:14 +0530

    Added: Navbar

---
+++ src/config/site.js
@@ -0,0 +1,30 @@
+export const siteConfig = {
+  name: "Portal",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Events",
+      href: "/",
+    },
+    {
+      title: "Zephr",
+      href: "/",
+    },
+    {
+      title: "Concession",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+};
+
+export const SiteConfig = siteConfig;
