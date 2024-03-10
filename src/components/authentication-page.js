984780d79184d368f002c2f376d9cf9bcd6edc42
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-10 14:02:00 +0530

    Done With Hero

--- src/components/authentication-page.js
+++ src/components/authentication-page.js
@@ -34,8 +34,8 @@ export default function AuthenticationPage() {
         >
           Login
         </p>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative hidden h-full flex-col bg-muted p-10 lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-muted" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
