efb24037b61b32b64a25c0839ac4e1adfeaec3a3
akashgupta200134 <akashgupta_ce_2021@ltce.in>
2024-03-23 02:55:10 +0530

    clubs added

--- src/app/clubs/page.js
+++ src/app/clubs/page.js
@@ -1,7 +1,152 @@
+import { Club } from "lucide-react";
 import React from "react";
+import Cesalogo from  "../../images/cesalogo.png";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Clubs = () => {
-  return <div>Clubs</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-2 mt-10">
+        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl text-center">
+          Student Clubs
+        </h1>
+        <p className="text-xl font-medium text-gray-500 text-center">
+          DISCOVER DIFFERENT TEAMS that will infuse vitality into your college
+          experience
+        </p>
+      </div>
+
+
+
+      <div className= " grid md:grid-cols-3 justify-center items-center md:ml-[135px]  mr-12 grid-cols-1 gap-2 ">
+
+     
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <a href="https://www.instagram.com/your-instagram-account">
+              <FaInstagram className="border h-12 w-12 px-2 py-2 rounded-full" />
+            </a>
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+      
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+      
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+     
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+        
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+       
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+       
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+       
+      </div>
+
+      <div className="border h-auto w-64 mt-28">
+        <img src={Cesalogo} alt="Cesa Logo" />
+        <p className="text-center font-medium text-lg ml-5 mr-5">
+          Computer Engineering Student Association (CESA)
+        </p>
+        <div className=" flex flex-row items-center justify-center mt-12 gap-3 mb-4">
+        <FaInstagram className=" border h-12 w-12 px-2 py-2 rounded-full " />
+        <FaFacebookF className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        <FaXTwitter className=" border h-12 w-12 px-2 py-2 rounded-full" />
+        </div>
+     
+      </div>
+
+
+
+</div>
+    </div>
+  );
 };
 
-export default Clubs ;
+export default Clubs;
