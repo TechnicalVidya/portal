3bfafe1ade7f2700b741a3cc887aa2ed62cbcde4
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-27 07:07:22 +0000

    clubs

--- src/modules/events/index.js
+++ src/modules/events/index.js
@@ -5,8 +5,6 @@ import img1 from '@/assets/1.jpg'
 import img2 from '@/assets/2.jpg'
 import img3 from '@/assets/3.jpg'
 
-
-
 const Events = () => {
     const imgArray = [img1, img2, img3]
     return (
