1d7cd1ac8bbc505d94009019ae9a492dd214466c
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-27 07:01:55 +0000

    added slider

---
+++ src/modules/clubs/index.js
@@ -0,0 +1,21 @@
+
+import { CarouselDemo } from '@/components/carousel'
+import React from 'react'
+import img1 from '@/assets/1.jpg'
+import img2 from '@/assets/2.jpg'
+import img3 from '@/assets/3.jpg'
+
+
+
+const Clubs = () => {
+  const imgArray = [img1, img2, img3]
+  return (
+    <div>
+      <div className='flex items-center justify-center'>   <CarouselDemo imgArray={imgArray} />
+      </div>
+
+    </div>
+  )
+}
+
+export default Clubs
