3863bd89732d74339ac74988800b048baf58e128
Roneet Yadav <116804447+Dev-X-Innovate@users.noreply.github.com>
2024-03-27 21:47:40 +0530

    Major Changes in Ui

--- src/config/site.js
+++ src/config/site.js
@@ -1,6 +1,5 @@
 export const siteConfig = {
   name: "INGENIA",
-  name: "Ingenia",
   description:
     "Where we're all about making memories that'll make your Instagram jealous. Join the fun as we turn your events into legendary stories.",
   mainNav: [
