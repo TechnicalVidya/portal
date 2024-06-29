556894c0a43fe9333be45be768e91c918144a2fe
Ansh Varma <anshvarma83691@gmail.com>
2024-06-29 20:00:14 +0530

    err

--- src/modules/zephyr/index.js
+++ src/modules/zephyr/index.js
@@ -7,8 +7,10 @@ import img1 from "@/assets/zephyrImg/IMG_1.JPG";
 import img2 from "@/assets/zephyrImg/IMG_2.JPG";
 import img3 from "@/assets/zephyrImg/IMG_3.png";
 import img4 from "@/assets/zephyrImg/IMG_4.png";
+// import Heading from "@/components/ui/heading";
 import Heading2 from "@/components/ui/heading2"
-// import { VelocityScroll } from "@/components/ui/velocityTextScroll";
+// import { Badge } from "@/components/ui/badge";
+// import { VelocityScroll } from "@/components/ui/velocityTextScroll"; 
 
 
 
@@ -28,19 +30,15 @@ const Zephyr = () => {
 
 
 
-    <div className="pt-5 grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
       <WobbleCard
         containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
         className=""
       >
         <div className="max-w-xs">
-          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Gippity AI powers the entire universe
-          </h2>
-          <p className="mt-4 text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
-          </p>
+          <h1 className=" p-10 max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            The Cult
+          </h1>
         </div>
         <Image
           src="/linear.webp"
@@ -50,24 +48,16 @@ const Zephyr = () => {
           className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
         />
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 min-h-[300px]">
-        <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
-        </h2>
-        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
-        </p>
+      <WobbleCard containerClassName="col-span-1 min-h-[300px] bg-pink-800 ">
+      <h1 className=" p-10 max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Sports
+          </h1>
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
         <div className="max-w-sm">
-          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
-          </h2>
-          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
-          </p>
+        <h1 className=" p-10 max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Technical
+          </h1>
         </div>
         <Image
           src="/linear.webp"
