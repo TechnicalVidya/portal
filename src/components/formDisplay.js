78ed5e13f79b3b78a03ea71e99f7691e345ae85c
OmkarAnbhule <omkaranbhule75@gmail.com>
2024-09-15 00:04:15 +0530

    update concession

--- src/components/formDisplay.js
+++ src/components/formDisplay.js
@@ -30,28 +30,32 @@ export default function FormDisplay({ data, loading }) {
                 <CardLoading />
             </div>
         );
-    return (
-        <div className="container mx-auto px-4 md:px-0">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl">
-                <Card className='min-w-[300px]'>
-                    <CardHeader>
-                        <CardTitle className='line-clamp-1 mb-3 font-serif'>
-                            {data.boarding}
-                        </CardTitle>
-                        <CardDescription className={`w-max h-max p-2 rounded-2xl ${data.status == 'pending' ? 'bg-yellow-200 text-yellow-600' : data.status == 'approved' ? 'bg-green-200 text-green-700' : 'text-red-700 bg-red-200'}`}>
-                            {data.status}
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p className='text-md font-semibold'>Class:&nbsp;&nbsp;<b>{data.className}</b></p>
-                        <p className='text-md font-semibold'>Duration:&nbsp;&nbsp;<b>{data.duration}</b></p>
-                        <p className='text-md font-semibold'>Line:&nbsp;&nbsp;<b>{data.line}</b></p>
-                    </CardContent>
-                    <CardFooter>
-                        <p className='text-md font-semibold'>{new Date(data.createdAt).toLocaleDateString()}</p>
-                    </CardFooter>
-                </Card>
+    if (data.boarding)
+        return (
+            <div className="container mx-auto px-4 md:px-0">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl">
+                    <Card className='min-w-[300px]'>
+                        <CardHeader>
+                            <CardTitle className='line-clamp-1 mb-3 font-serif'>
+                                {data.boarding}
+                            </CardTitle>
+                            <CardDescription className={`w-max h-max p-2 rounded-2xl ${data.status == 'pending' ? 'bg-yellow-200 text-yellow-600' : data.status == 'approved' ? 'bg-green-200 text-green-700' : 'text-red-700 bg-red-200'}`}>
+                                {data.status}
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p className='text-md font-semibold'>Class:&nbsp;&nbsp;<b>{data.className}</b></p>
+                            <p className='text-md font-semibold'>Duration:&nbsp;&nbsp;<b>{data.duration}</b></p>
+                            <p className='text-md font-semibold'>Line:&nbsp;&nbsp;<b>{data.line}</b></p>
+                        </CardContent>
+                        <CardFooter>
+                            <p className='text-md font-semibold'>{new Date(data.createdAt).toLocaleDateString()}</p>
+                        </CardFooter>
+                    </Card>
+                </div>
             </div>
-        </div>
+        )
+    return (
+        <p className='text-center w-full font-semibold text-xl'>No forms found</p>
     )
 }
