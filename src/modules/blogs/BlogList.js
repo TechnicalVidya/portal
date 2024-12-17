f834550cc7434f0e9a9ac7338593ce941e47a27a
OmkarAnbhule <omkaranbhule75@gmail.com>
2024-12-17 09:21:34 +0530

    update blogs

--- src/modules/blogs/BlogList.js
+++ src/modules/blogs/BlogList.js
@@ -1,65 +1,3 @@
-//"use client";
-/*
-import { useState, useEffect } from "react";
-import BlogCard from "./BlogCard";
-import BlogForm from "./BlogForm";
-import { fetchAllBlogs, createBlog, updateBlog } from "@/utils/blog";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-
-export default function BlogList() {
-  const [blogs, setBlogs] = useState([]);
-  const [editingBlog, setEditingBlog] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const handleSave = async (updatedBlog) => {
-    setLoading(true);
-    try {
-      if (editingBlog) {
-        const updatedData = await updateBlog(editingBlog.id, updatedBlog);
-
-        setBlogs(blogs.map((b) => (b.id === editingBlog.id ? updatedData : b)));
-      } else {
-        // Create new blog in the backend
-        const newBlog = await createBlog(updatedBlog, setLoading);
-        setBlogs([newBlog, ...blogs]);
-      }
-
-      setEditingBlog(null);
-    } catch (error) {
-      console.error("Failed to save blog:", error);
-      alert("Failed to save blog. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <div>
-      <Dialog>
-        <DialogTrigger className="bg-black text-white rounded-lg p-2">
-          Add New Blog
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle className="text-4xl font-bold text-center my-8">
-              ADMIN DASHBOARD
-            </DialogTitle>
-            <DialogDescription>
-              <BlogForm blogData={editingBlog} onSuccess={handleSave} />
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
-}*/
-
 "use client";
 
 import { useState } from "react";
@@ -80,7 +18,7 @@ export default function BlogList({ editingBlog, onSave, onCancel , isOpen , setI
     setLoading(true);
     try {
       if (editingBlog) {
-        const updatedData = await updateBlog(editingBlog.id, updatedBlog); // Assuming updateBlog handles the update
+        const updatedData = await updateBlog(editingBlog.id, updatedBlog);
         onSave(updatedData);
       } else {
         const newBlog = await createBlog(updatedBlog, setLoading);
