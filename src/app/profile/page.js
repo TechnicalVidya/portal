77431deae77f74b9ef19cdcb15fb7b7660d4c2ee
Technical-Vidya <techvidya1905@gmail.com>
2025-01-18 14:35:04 +0000

    update fees receipt --deployment

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,20 +2,21 @@
 import Profile from "@/modules/profile";
 import { pdfjs } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
-
 if (typeof Promise.withResolvers === 'undefined') {
-  Promise.withResolvers = function () {
-    let resolve;
-    let reject;
-    const promise = new Promise((res, rej) => {
-      resolve = res;
-      reject = rej;
-    });
-    return { promise, resolve, reject };
-  };
+    Promise.withResolvers = function () {
+        let resolve;
+        let reject;
+        const promise = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+        });
+        return { promise, resolve, reject };
+    };
 }
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
+
+
 export default function Page() {
-  return <Profile />;
+    return <Profile />;
 }
