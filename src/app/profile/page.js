4b36b8e0ad7dc513b0c1353ab2e6611c98820fb7
Technical-Vidya <techvidya1905@gmail.com>
2025-01-18 14:12:26 +0000

    update fees receipt --deployment

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,8 +3,8 @@ import Profile from "@/modules/profile";
 import { pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
+    'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+    import.meta.url,
 ).toString();
 
 export default function Page() {
