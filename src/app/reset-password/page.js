74d5c5bd70b1adf8679d23da78709ac950c598ce
Technical-Vidya <techvidya1905@gmail.com>
2025-01-18 14:44:09 +0000

    update fees receipt --deployment

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,14 +1,17 @@
 "use client";
 import { useSearchParams } from 'next/navigation';
 import ResetPasswordForm from "@/components/hero/resetPassword-form";
+import { Suspense } from 'react';
 
 export default function ResetPasswordPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
   return (
-    <div className="grid place-items-center h-screen">
-      <ResetPasswordForm token={token} />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="grid place-items-center h-screen">
+        <ResetPasswordForm token={token} />
+      </div>
+    </Suspense>
   );
 }
